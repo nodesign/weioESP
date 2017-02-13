69f9b74ed3b62e003c67ccc03f78cab4f3fb49f5
Uros Petrevski <uros@nodesign.net>
2017-02-13 17:53:53 +0100

    Electrolink JS library

--- html/lib/electrolink.js
+++ html/lib/electrolink.js
@@ -1,77 +1,94 @@
-var BROKER_ADDRESS = "ws://XXX.XXX.XXX.XXX:4444";
-var REQUEST_TOPIC = "weio_command";
-var ANSWER_TOPIC = "weio_reply";
-var ERROR_TOPIC = "weio_error";
+(function(window){
+    'use strict';
+    function define_Electrolink(){
+        var Electrolink = {};
 
-var OUT = 0;
-var IN  = 1;
-var PULL_UP = 2;
-var PULL_DOWN = 3;
+        var BROKER_ADDRESS = "ws://XXX.XXX.XXX.XXX:4444";
+        var REQUEST_TOPIC = "weio_command";
+        var ANSWER_TOPIC = "weio_reply";
+        var ERROR_TOPIC = "weio_error";
 
-/**
- * Define callbacks here and request keys
- * Each key is binded to coresponding function
- */
-var weioCallbacks = {};
+        /**
+         * Define callbacks here and request keys
+         * Each key is binded to coresponding function
+         */
+        var weioCallbacks = {};
 
+        var client = mqtt.connect(BROKER_ADDRESS); // you add a ws:// url here
 
-var client = mqtt.connect(BROKER_ADDRESS); // you add a ws:// url here
+        client.subscribe(ANSWER_TOPIC);
+        client.subscribe(ERROR_TOPIC);
 
-client.subscribe(ANSWER_TOPIC);
-client.subscribe(ERROR_TOPIC);
+        client.on("message", function (topic, payload) {
+            var data = JSON.parse(payload);
+            if (topic == ANSWER_TOPIC) {
 
-client.on("message", function (topic, payload) {
-    var data = JSON.parse(payload);
-    if (topic == ANSWER_TOPIC) {
+                var instruction = data.value[1];
+                if (instruction in weioCallbacks) {
+                    weioCallbacks[instruction](data.value);
+                }
 
-        instruction = data.value[1];
-        if (instruction in weioCallbacks) {
-            weioCallbacks[instruction](data.value);
+                } else if (topic == ERROR_TOPIC) {
+                    var err = JSON.parse(payload);
+                    console.log("ERROR",err);
+                }
+                //client.end();
+            });
+
+        Electrolink.OUT = 0;
+        Electrolink.IN  = 1;
+        Electrolink.PULL_UP = 2;
+        Electrolink.PULL_DOWN = 3;
+
+        // PWM
+        Electrolink.pwmStart = function(pwmNb, frequence) {
+            Electrolink.sender("pwmStart", [pwmNb, frequence]);
         }
 
-        } else if (topic == ERROR_TOPIC) {
-            var err = JSON.parse(payload);
-            console.log("ERROR",err);
+        Electrolink.pwmSet = function(pwmNb, duty) {
+            Electrolink.sender("pwmSet", [pwmNb, duty]);
         }
-        //client.end();
-    });
 
-// PWM
-function pwmStart(pwmNb, frequence) {
-    sender("pwmStart", [pwmNb, frequence]);
-}
+        Electrolink.pwmStop = function(pwmNb) {
+            Electrolink.sender("pwmStop", [pwmNb]);
+        }
 
-function pwmSet(pwmNb, duty) {
-    sender("pwmSet", [pwmNb, duty]);
-}
+        // DIGITAL GPIO
 
-function pwmStop(pwmNb) {
-    sender("pwmStop", [pwmNb]);
-}
+        Electrolink.pinMode = function(pinId, mode) {
+            Electrolink.sender("pinMode", [pinId, mode]);
+        }
 
-// DIGITAL GPIO
+        Electrolink.digitalWrite = function(pinId, value) {
+            Electrolink.sender("digitalWrite", [pinId, value]);
+        }
 
-function pinMode(pinId, mode) {
-    sender("pinMode", [pinId, mode]);
-}
+        Electrolink.digitalRead = function(pinId, callback) {
+            // create new callback call
+            var fName = callback.name;
+            //console.log("callback name:" + fName);
+            weioCallbacks[fName] = callback
+            // pass callback name to match it with answer
+            // callback name will be returned with answer
+            // in that way it's possible to define multiple
+            // digitalReads with different callbacks
+            Electrolink.sender("digitalRead", [pinId, fName]); 
+        }
 
-function digitalWrite(pinId, value) {
-    sender("digitalWrite", [pinId, value]);
-}
+        Electrolink.sender = function(method, params) {
+            var data = {"method":method, "params":params};
+            client.publish(REQUEST_TOPIC, JSON.stringify(data));
+        }
+        
+        
+        return Electrolink;
+    }
+    //define globally if it doesn't already exist
+    if(typeof(Electrolink) === 'undefined'){
+        window.Electrolink = define_Electrolink();
+    }
+    else{
+        console.log("Electrolink library already defined!");
+    }
+})(window);
 
-function digitalRead(pinId, callback) {
-    // create new callback call
-    var fName = callback.name;
-    //console.log("callback name:" + fName);
-    weioCallbacks[fName] = callback
-    // pass callback name to match it with answer
-    // callback name will be returned with answer
-    // in that way it's possible to define multiple
-    // digitalReads with different callbacks
-    sender("digitalRead", [pinId, fName]); 
-}
-
-function sender(method, params) {
-    data = {"method":method, "params":params};
-    client.publish(REQUEST_TOPIC, JSON.stringify(data));
-}
--- html/sketch.js
+++ html/sketch.js
@@ -2,9 +2,14 @@ var h = 600;
 var light = 0;
 
 function setup() {
-    pwmStart(5,255);
-    pinMode(0, IN);
-    digitalRead(0, readCallback);
+    Electrolink.pwmStart(5,255);
+    Electrolink.pinMode(0, Electrolink.IN);
+    
+    Electrolink.digitalRead(0,  function readCallback(data) {
+
+        console.log("read from pin",data);
+
+    });
     // canvas size in pixels
     createCanvas(windowWidth, windowHeight);
     // always put some frame rate, don't make suffer cpu or websockets
@@ -23,18 +28,12 @@ function draw() {
     ellipse(width/2.0, height/2.0, p, height);
     light = parseInt(map(h, 60, height-60, 255,0));
 
-    ellipse(100,100, 40,40);
-
 
 }
 
 function touchMoved() {
   h = mouseY;
-  pwmSet(5,light);
+  Electrolink.pwmSet(5,light);
   // prevent default
   return false;
-}
-
-function readCallback(data) {
-    console.log("read",data);
 }
